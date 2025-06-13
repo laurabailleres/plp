6130d7b556a9b28e7f14a85d42c47ae524c45684
laura bailleres <88726253+laurabailleres@users.noreply.github.com>
2025-06-13 12:10:30 -0300

    Add files via upload

--- guias/guia8/todos.pl
+++ guias/guia8/todos.pl
@@ -262,13 +262,9 @@ vacio(nil).
 % raiz(+B, ?R)
 raiz(bin(_, X, _), X).
 
-% max(+X, +Y, -M)
-max(X, Y, X) :- X>Y, !.
-max(X, Y, Y) :- Y>=X.
-
 % altura(+B, ?A)
 altura(nil, 0).
-altura(bin(I, _, D), A) :- altura(I,A1), altura(D,A2), max(A1, A2, M), A is M+1.
+altura(bin(I, _, D), A) :- altura(I,A1), altura(D,A2), A is max(A1, A2) + 1.
 
 % cantidadDeNodos(+B, ?N)
 cantidadDeNodos(nil, 0).
