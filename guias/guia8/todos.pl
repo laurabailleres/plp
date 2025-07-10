9708774a69d03f5ff264a0182051a44b97d25d65
laura bailleres <88726253+laurabailleres@users.noreply.github.com>
2025-07-10 11:51:36 -0300

    Add files via upload

--- guias/guia8/todos.pl
+++ guias/guia8/todos.pl
@@ -339,6 +339,31 @@ aBBInsertar(X, T1, T2) :- insertar(X, T1, T2), aBB(T2).
 
 
 
-% ejercicio 13
+% EJERCICIO 13
 % coprimos(-X,-Y)
 coprimos(X, Y) :- between(1, inf, X), between(1, X, Y), 1 is gcd(X, Y).
+
+
+
+% EJERCICIO 14
+% ítem I
+% listaQueSuma(+S, +L, -XS)
+% S = número que tiene que sumar
+% L = largo de la lista
+% XS = lista
+listaQueSuma(0, 0, []).
+listaQueSuma(S, L, [X|XS]) :- L > 0, between(0, S, X), S1 is S-X, L1 is L-1, listaQueSuma(S1, L1, XS).
+
+% matrizQueSuma(+S, +F, +C, -XS)
+% S = lo que tiene que sumar cada lista
+% F = cantidad de listas (filas)
+% C = largo de las listas (columnas)
+% XS = matriz
+matrizQueSuma(_, 0, _, []).
+matrizQueSuma(S, F, C, [X|XS]) :- F > 0, listaQueSuma(S, C, X), F1 is F-1, matrizQueSuma(S, F1, C, XS).
+
+% cuadradoSemiMágico(+N, -XS)
+cuadradoSemiMagico(N, XS) :- between(0, inf, M), matrizQueSuma(M, N, N, XS).
+% instancio la cantidad de filas y de columnas con el mismo número (el tamaño del
+% cuadrado, o sea N) porque quiero justamente que sea un cuadrado. con between
+% genero naturales y con eso genero cuadrados semimágicos.
